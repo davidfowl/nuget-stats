d851c81a74895699fdfa6a3791469d364af6d3c7
David Fowler <davidfowl@gmail.com>
2011-02-14 02:24:49 -0800

    Cache more aggressively and update page less.

---
+++ App_Code/PackageRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Caching;
+using NuGet;
+
+public class PackageRepository {
+    private static readonly object _lockObject = new object();
+    private static readonly DataServicePackageRepository _repository = new DataServicePackageRepository(new Uri("http://packages.nuget.org/v1/FeedService.svc"));
+
+    public static IList<IPackage> GetPackages(Cache cache) {
+        // Try to load if from the cache
+        var packages = (IList<IPackage>)cache.Get("packages");
+
+        // Double check lock
+        if (packages == null) {
+            lock (_lockObject) {
+                packages = (IList<IPackage>)cache.Get("packages");
+
+                if (packages == null) {
+                    // If we still don't have anything cached then get the package list and store it.
+                    packages = _repository.GetPackages().ToList();
+
+                    cache.Insert("packages", 
+                                  packages, 
+                                  null, 
+                                  DateTime.Now + TimeSpan.FromSeconds(30), 
+                                  Cache.NoSlidingExpiration);
+                }
+            }
+        }
+
+        return packages;
+    }
+}
