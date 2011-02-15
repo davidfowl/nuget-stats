e211903a07fbbfc81b694061c2a9b57ad03e42f9
David Fowler <davidfowl@gmail.com>
2011-02-15 00:32:15 -0800

    Show last 5 updated packages.

--- App_Code/PackageRepository.cs
+++ App_Code/PackageRepository.cs
@@ -8,23 +8,23 @@ public class PackageRepository {
     private static readonly object _lockObject = new object();
     private static readonly DataServicePackageRepository _repository = new DataServicePackageRepository(new Uri("http://packages.nuget.org/v1/FeedService.svc"));
 
-    public static IList<IPackage> GetPackages(Cache cache) {
+    public static IList<DataServicePackage> GetPackages(Cache cache) {
         // Try to load if from the cache
-        var packages = (IList<IPackage>)cache.Get("packages");
+        var packages = (IList<DataServicePackage>)cache.Get("packages");
 
         // Double check lock
         if (packages == null) {
             lock (_lockObject) {
-                packages = (IList<IPackage>)cache.Get("packages");
+                packages = (IList<DataServicePackage>)cache.Get("packages");
 
                 if (packages == null) {
                     // If we still don't have anything cached then get the package list and store it.
-                    packages = _repository.GetPackages().ToList();
+                    packages = _repository.GetPackages().AsEnumerable().Cast<DataServicePackage>().ToList();
 
-                    cache.Insert("packages", 
-                                  packages, 
-                                  null, 
-                                  DateTime.Now + TimeSpan.FromSeconds(20), 
+                    cache.Insert("packages",
+                                  packages,
+                                  null,
+                                  DateTime.Now + TimeSpan.FromSeconds(20),
                                   Cache.NoSlidingExpiration);
                 }
             }
