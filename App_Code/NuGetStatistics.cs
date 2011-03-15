8ce2f5dce3b77297c2f35d0b184e26dca57c96f8
pranavkm <prkrishn@microsoft.com>
2011-03-14 18:27:59 -0700

    Updating NuGetStats to log values every 30 minutes. Change meta statistic calculation to use GetUTCDate

--- App_Code/NuGetStatistics.cs
+++ App_Code/NuGetStatistics.cs
@@ -12,11 +12,10 @@ public static class NuGetStatistics {
         using (var db = Database.Open("Stats")) {
             DateTime? lastLog = db.QueryValue("Select top 1 LogTime from Stats order by LogTime desc");
 
-            if (!lastLog.HasValue || DateTime.UtcNow.Subtract(lastLog.Value).TotalMinutes > 45) {
+            if (!lastLog.HasValue || DateTime.UtcNow.Subtract(lastLog.Value).TotalMinutes > 30) {
                 var stats = PackageRepository.GetCurrentStatistics(cache);
 
-                db.Execute("Insert into Stats (LogTime, Downloads, UniquePackages, TotalPackages) values (@0, @1, @2, @3)",
-                    DateTime.UtcNow,
+                db.Execute("Insert into Stats (LogTime, Downloads, UniquePackages, TotalPackages) values (GETUTCDATE(), @0, @1, @2)",
                     stats.TotalDownloads,
                     stats.UniqueCount,
                     stats.TotalCount);
@@ -49,8 +48,8 @@ public static class NuGetStatistics {
     public static MetaStatistics GetMetaStatistics(Statistics stats) {
         var metaStats = new MetaStatistics();
         using (var db = Database.Open("Stats")) {
-            int? hourDownloads = stats.TotalDownloads - (int?)db.QueryValue("Select top 1 Downloads from Stats where LogTime < DateAdd(hh, -1, GetDate()) order by LogTime desc");
-            int? dayPackages = stats.TotalCount - (int?)db.QueryValue("Select top 1 TotalPackages from Stats where LogTime < DateAdd(day, -1, GetDate()) order by LogTime desc");
+            int? hourDownloads = stats.TotalDownloads - (int?)db.QueryValue("Select top 1 Downloads from Stats where LogTime < DateAdd(hh, -1, GETUTCDATE())  order by LogTime desc");
+            int? dayPackages = stats.TotalCount - (int?)db.QueryValue("Select top 1 TotalPackages from Stats where LogTime < DateAdd(day, -1, GETUTCDATE()) order by LogTime desc");
 
             if (dayPackages.HasValue) {
                 metaStats.DayPackages = dayPackages.Value;
